dcbd8dee16bba2d4908c858b76ad1b6885c64650
Muhamad Rizal Arfiyan <rizal.arfiyan.23@gmail.com>
2022-06-16 05:40:36 +0700

    feat: add webpack, babel, nodemon

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const path = require('path');
+const nodeExternals = require("webpack-node-externals");
+require("dotenv").config();
+const CURRENT_WORKING_DIR = process.cwd();
+
+module.exports = {
+  name: "server",
+  target: "node",
+  context: path.resolve(CURRENT_WORKING_DIR, "src"),
+  entry: {
+    server: "./index.js",
+  },
+  mode: "development",
+  target: "node",
+  node: {
+    __dirname: false,
+    __filename: false,
+  },
+  externals: [nodeExternals()],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: [/(node_modules)/],
+        loader: "babel-loader",
+        options: {
+          configFile: path.resolve(CURRENT_WORKING_DIR, ".babelrc"),
+        },
+      },
+    ],
+  },
+  output: {
+    path: path.resolve(CURRENT_WORKING_DIR, "dist"),
+    filename: "[name].bundle.js",
+    libraryTarget: "commonjs2",
+  },
+  devtool: "source-map",
+};
